b2b922addfda4cac1808ffb9602ab406934e14da
RishikaChaturvedi-cloud <ruxh1chaubey@gmail.com>
2026-05-17 12:38:52 +0530

    vectordb.py

--- data/rag/vectordb.py
+++ data/rag/vectordb.py
@@ -1,1 +1,11 @@
+from langchain_community.vectorstores import Chroma
 
+def create_vector_db(docs, embeddings):
+
+    db = Chroma.from_documents(
+        docs,
+        embeddings,
+        persist_directory="db"
+    )
+
+    return db
