f6e046afd9687335efcd256be12c8d548a40ba7c
RishikaChaturvedi-cloud <ruxh1chaubey@gmail.com>
2026-05-17 12:36:53 +0530

    embeddings.py

--- data/rag/embeddings.py
+++ data/rag/embeddings.py
@@ -1,1 +1,12 @@
+from dotenv import load_dotenv
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
+load_dotenv()
+
+def get_embeddings():
+
+    embeddings = GoogleGenerativeAIEmbeddings(
+        model="models/embedding-001"
+    )
+
+    return embeddings
