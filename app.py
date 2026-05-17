7c80e71fad3b79e70359843039f2ead673e9c7f2
RishikaChaturvedi-cloud <ruxh1chaubey@gmail.com>
2026-05-17 11:45:59 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,78 @@
+import os
+import streamlit as st
+from dotenv import load_dotenv
+
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+from langchain_google_genai import (
+    GoogleGenerativeAIEmbeddings,
+    ChatGoogleGenerativeAI
+)
+
+from langchain_community.vectorstores import Chroma
+from langchain.chains.question_answering import load_qa_chain
+
+# Load API key
+load_dotenv()
+
+# Streamlit UI
+st.set_page_config(page_title="Law & Policy Decoder")
+
+st.title("🇮🇳 Law & Policy Decoder")
+st.write("Ask questions about Indian government schemes")
+
+# Load PDF
+loader = PyPDFLoader("data/pm_kisan.pdf")
+documents = loader.load()
+
+# Split text
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=200
+)
+
+docs = splitter.split_documents(documents)
+
+# Embeddings
+embeddings = GoogleGenerativeAIEmbeddings(
+    model="models/embedding-001"
+)
+
+# Vector DB
+db = Chroma.from_documents(
+    docs,
+    embeddings,
+    persist_directory="db"
+)
+
+retriever = db.as_retriever(search_kwargs={"k": 3})
+
+# Gemini Model
+llm = ChatGoogleGenerativeAI(
+    model="gemini-1.5-flash",
+    temperature=0.3
+)
+
+chain = load_qa_chain(llm, chain_type="stuff")
+
+# User input
+question = st.text_input("Enter your question")
+
+if question:
+
+    relevant_docs = retriever.get_relevant_documents(question)
+
+    response = chain.run(
+        input_documents=relevant_docs,
+        question=question
+    )
+
+    st.subheader("Answer")
+    st.write(response)
+
+    st.subheader("Sources")
+
+    for doc in relevant_docs:
+        st.write(doc.page_content[:500])
+        st.write("------")
