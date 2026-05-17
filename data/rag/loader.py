6194f0c72fbef8f18c354a7d3984b26a48e79303
RishikaChaturvedi-cloud <ruxh1chaubey@gmail.com>
2026-05-17 12:37:31 +0530

    loader.py

--- data/rag/loader.py
+++ data/rag/loader.py
@@ -1,1 +1,20 @@
+import os
+from langchain_community.document_loaders import PyPDFLoader
 
+def load_documents():
+
+    documents = []
+
+    folder_path = "data"
+
+    for file in os.listdir(folder_path):
+
+        if file.endswith(".pdf"):
+
+            pdf_path = os.path.join(folder_path, file)
+
+            loader = PyPDFLoader(pdf_path)
+
+            documents.extend(loader.load())
+
+    return documents
