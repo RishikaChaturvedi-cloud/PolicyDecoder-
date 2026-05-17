4551c9b7b1759907573df551715ef2671cdb8437
RishikaChaturvedi-cloud <ruxh1chaubey@gmail.com>
2026-05-17 12:38:12 +0530

    splitter.py

--- data/rag/splitter.py
+++ data/rag/splitter.py
@@ -1,1 +1,12 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
+def split_documents(documents):
+
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200
+    )
+
+    docs = splitter.split_documents(documents)
+
+    return docs
